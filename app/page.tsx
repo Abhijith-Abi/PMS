27a66d42f40e8f0135d47a385acca24f05991417
abhijith-pa <abhijith@steyp.com>
2024-07-14 18:05:52 +0530

    fiet:     add shard cn and componet     then make custom input components     add icon and images     add styled and tailwind configs
    Changes to be committed:
    	modified:   app/page.tsx
    	new file:   components/CustomFormField.tsx
    	new file:   components/forms/PatientForm.tsx
    	new file:   components/ui/form.tsx
    	new file:   components/ui/input.tsx
    	new file:   components/ui/label.tsx
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   public/assets/gifs/success.gif
    	new file:   public/assets/icons/appointments.svg
    	new file:   public/assets/icons/arrow.svg
    	new file:   public/assets/icons/calendar.svg
    	new file:   public/assets/icons/cancelled.svg
    	new file:   public/assets/icons/check-circle.svg
    	new file:   public/assets/icons/check.svg
    	new file:   public/assets/icons/close.svg
    	new file:   public/assets/icons/email.svg
    	new file:   public/assets/icons/loader.svg
    	new file:   public/assets/icons/logo-full.svg
    	new file:   public/assets/icons/logo-icon.svg
    	new file:   public/assets/icons/pending.svg
    	new file:   public/assets/icons/upload.svg
    	new file:   public/assets/icons/user.svg
    	new file:   public/assets/images/admin.png
    	new file:   public/assets/images/appointment-img.png
    	new file:   public/assets/images/appointments-bg.png
    	new file:   public/assets/images/cancelled-bg.png
    	new file:   public/assets/images/dr-cameron.png
    	new file:   public/assets/images/dr-cruz.png
    	new file:   public/assets/images/dr-green.png
    	new file:   public/assets/images/dr-lee.png
    	new file:   public/assets/images/dr-livingston.png
    	new file:   public/assets/images/dr-peter.png
    	new file:   public/assets/images/dr-powell.png
    	new file:   public/assets/images/dr-remirez.png
    	new file:   public/assets/images/dr-sharma.png
    	new file:   public/assets/images/onboarding-img.png
    	new file:   public/assets/images/pending-bg.png
    	new file:   public/assets/images/register-img.png
    	new file:   public/favicon.ico

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl underline text-[#fff]">Home</h1>
-      <Button>Hello</Button>
-    </>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[497px]">
+          <Image
+            src="assets/icons/logo-full.svg"
+            alt="log-img"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex  justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              {" "}
+              @ 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="hero-image"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
